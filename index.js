c4d54c9b5efd046542ca5d220df7efd4e9ecdafc
eslam <121362570+loma2023@users.noreply.github.com>
2026-09-10 01:38:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ quiet: true });
 const connectDB = require("./Config/db");
 const seedAccounts = require("./Config/SeedChartOfAccounts");
 const cors = require("cors");
